e8a182882642e162ba741e1b0fa4c6fa6e41ce21
양세정 <yangsejeong@yangsejeong-ui-MacBook-Air.local>
2020-10-29 03:30:22 +0900

    adding maps done

--- seeds/index.js
+++ seeds/index.js
@@ -24,15 +24,25 @@ const seedDB = async() => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
+            //YOUR USER ID
             author: '5f993e934e2ebeeeb35a842d',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
             price,
+            geometry: {
+                type: "Point",
+                coordinates: [-113.1331, 47.0202]
+            },
             images: [{
-                url: 'https://res.cloudinary.com/dmgdudvaq/image/upload/v1603898828/YelpCamp/ljkxlspiftrh4c9g3swh.jpg',
-                filename: 'YelpCamp/ljkxlspiftrh4c9g3swh'
-            }]
+                    url: 'https://res.cloudinary.com/dmgdudvaq/image/upload/v1603898828/YelpCamp/ljkxlspiftrh4c9g3swh.jpg',
+                    filename: 'YelpCamp/ljkxlspiftrh4c9g3swh'
+                },
+                {
+                    url: 'https://res.cloudinary.com/dmgdudvaq/image/upload/v1603908423/YelpCamp/dcutdpakavcsq3ohiukr.jpg',
+                    filename: 'YelpCamp/dcutdpakavcsq3ohiukr'
+                }
+            ]
         })
         await camp.save();
     }
